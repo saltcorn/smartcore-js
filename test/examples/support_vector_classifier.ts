ece2f83000a509e11fb3d797bf3e6cab11fd3cd4
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-09-12 15:13:38 +0300

    broken:saving progress of attempt to implement bindings for SVC.

---
+++ test/examples/support_vector_classifier.ts
@@ -0,0 +1,16 @@
+import { dataset, trainTestSplitF32U32, R2U32, SVCF32U32, SVCParametersF32 } from '../../index'
+import assert from 'assert'
+
+export default () => {
+  it('Support Vector Classifier (SVC)', () => {
+    let loadedDataset = dataset.breastCancer().loadDataset()
+    let matrix = loadedDataset.denseMatrix()
+    let y = loadedDataset.target
+    let [, x_test, , y_test] = trainTestSplitF32U32(matrix, y, 0.2, true)
+    let parameters = new SVCParametersF32()
+    let y_hat_lr = SVCF32U32.fit(matrix, loadedDataset.target, parameters).predict(x_test)
+    let r2 = new R2U32()
+    let score = r2.getScore(y_test, y_hat_lr)
+    assert(score)
+  })
+}
