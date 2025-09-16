09877b94e80c3ee92ed87d7b53a7b83c98cc7501
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-09-16 13:32:41 +0300

    Reproduced data splitting example, into training, validation, and testing data, in the model selection and evaluation section of the smartcore guide.

--- test/examples/index.spec.ts
+++ test/examples/index.spec.ts
@@ -1,9 +1,11 @@
 import quick_start from './quick_start'
 import supervised_learning from './supervised_learning'
 import unsupervised_learning from './unsupervised_learning'
+import model_selection from './model_selection'
 
 describe('Examples', () => {
   quick_start()
   supervised_learning()
   unsupervised_learning()
+  model_selection()
 })
---
+++ test/examples/model_selection/index.ts
@@ -0,0 +1,7 @@
+import train_test_split from './train_test_split'
+
+export default () => {
+  describe('Model Selection and Evaluation', () => {
+    train_test_split()
+  })
+}
