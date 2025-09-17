7345b40be373d684de8758891809e0fda88eb69f
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-09-17 18:17:53 +0300

    WIP: Add custom test. Failing due to introduced reference type.

---
+++ test/custom/index.spec.ts
@@ -0,0 +1,14 @@
+import { DenseMatrixF32, trainTestSplitF32F32 } from '../../index'
+
+describe('', () => {
+  it('', () => {
+    const arrays = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ]
+    const jsmFlat = new Float32Array(arrays.flat())
+    const x = new DenseMatrixF32(arrays.length, arrays[0].length, jsmFlat)
+    const y = new Float32Array([7, 8, 9])
+    let [x_train, x_test, y_train, y_test] = trainTestSplitF32F32(x, y, 0.2, true)
+  })
+})
