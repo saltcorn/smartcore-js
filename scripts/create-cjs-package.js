0df43a6f1dd94f9b06068aef145cac71ea584503
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-10-06 18:14:20 +0300

    Add a package.json file to the ./dist/cjs folder to set the package type to commonjs

---
+++ scripts/create-cjs-package.js
@@ -0,0 +1,11 @@
+import { writeFileSync } from 'fs'
+import { mkdirSync } from 'fs'
+
+try {
+  mkdirSync('./dist/cjs', { recursive: true })
+} catch (e) {
+  // Don't throw if the directory already exists
+}
+
+writeFileSync('./dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }, null, 2))
+console.log('Created dist/cjs/package.json')
