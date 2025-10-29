d7fa94804e9e0dbb6ca18dfad9743d6faae99c39
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-10-29 20:36:24 +0300

    Remove package.json file added to the 'dist/core-bindings' folder.

--- scripts/create-cjs-package.js
+++ scripts/create-cjs-package.js
@@ -3,11 +3,9 @@ import { mkdirSync } from 'fs'
 
 try {
   mkdirSync('./dist-cjs', { recursive: true })
-  mkdirSync('./dist/core-bindings', { recursive: true })
 } catch (e) {
   // Don't throw if the directory already exists
 }
 
 writeFileSync('./dist-cjs/package.json', JSON.stringify({ type: 'commonjs' }, null, 2))
-writeFileSync('./dist/core-bindings/package.json', JSON.stringify({ type: 'commonjs' }, null, 2))
 console.log('Created dist-cjs/package.json')
