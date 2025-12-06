7a52c973a04bc8c3eeb02c5d054f6737e990cbb5
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-12-06 18:02:15 +0300

    Remove code in 'create-cjs-package.js' that overwrites 'core-bindings' generated for CommonJs with those generated for ESM.

--- scripts/create-cjs-package.js
+++ scripts/create-cjs-package.js
@@ -2,26 +2,6 @@ import { readdirSync, statSync, existsSync, mkdirSync, copyFileSync } from 'fs'
 import { writeFileSync } from 'fs'
 import path from 'path'
 
-function copyFolderSync(src, dest) {
-  if (!existsSync(dest)) {
-    mkdirSync(dest, { recursive: true })
-  }
-
-  const files = readdirSync(src)
-
-  for (const file of files) {
-    const srcPath = path.join(src, file)
-    const destPath = path.join(dest, file)
-    const stat = statSync(srcPath)
-
-    if (stat.isDirectory()) {
-      copyFolderSync(srcPath, destPath)
-    } else {
-      copyFileSync(srcPath, destPath)
-    }
-  }
-}
-
 try {
   mkdirSync('./dist-cjs', { recursive: true })
 } catch (e) {
@@ -30,5 +10,3 @@ try {
 
 writeFileSync('./dist-cjs/package.json', JSON.stringify({ type: 'commonjs' }, null, 2))
 console.log('Created dist-cjs/package.json')
-copyFolderSync('./src-js/core-bindings/', './dist-cjs/core-bindings/')
-console.log("Copied the auto-generated 'core-bindings' folder into the 'dist-cjs' folder.")
